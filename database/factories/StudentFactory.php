bb7ae574a8b84edf33eacf5076f622be8658cc37
Lucas de Souza <111311202+LucasSouzaa97@users.noreply.github.com>
2023-03-02 21:32:17 -0300

    add students factory

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Exception;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     * @throws Exception
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+            'phone' => fake()->phoneNumber(),
+            'address' => fake()->address(),
+            'created_at' => fake()->dateTime(),
+            'updated_at' => fake()->dateTime(),
+        ];
+    }
+}
