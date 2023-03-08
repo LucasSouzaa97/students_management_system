b5723d04dd87941a761d68ceb16e34bb92c06b2b
Lucas de Souza <111311202+LucasSouzaa97@users.noreply.github.com>
2023-03-08 08:44:50 -0300

    add classes resources

---
+++ app/Filament/Resources/ClassesResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ClassesResource\Pages;
+use App\Filament\Resources\ClassesResource\RelationManagers;
+use App\Models\Classes;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Actions\DeleteAction;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ClassesResource extends Resource
+{
+    protected static ?string $model = Classes::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    protected static ?string $navigationGroup = 'Management System';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->unique()
+                    ->autofocus()
+                    ->maxLength(255)
+                    ->placeholder('Enter the name of the Class'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->sortable()
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListClasses::route('/'),
+            'create' => Pages\CreateClasses::route('/create'),
+            'edit' => Pages\EditClasses::route('/{record}/edit'),
+        ];
+    }
+}
